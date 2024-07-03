6534a6a299ed1d63d933d10791a617fa73aea51a
AdnenMess <adnen_messaoudi@hotmail.com>
2024-07-03 12:45:03 +0100

    correct dim and fact

--- models/dim/dim_hosts_cleansed.sql
+++ models/dim/dim_hosts_cleansed.sql
@@ -1,17 +1,12 @@
 {{
   config(
     materialized = 'view'
-    )
-}} 
-WITH src_hosts AS (
-    SELECT
-        *
-    FROM
-        {{ ref('src_hosts') }}
-)
+  )
+}}
+WITH src_hosts AS ( SELECT * FROM {{ ref('src_hosts') }} )
 SELECT
     host_id,
-    NVL(
+    COALESCE(
         host_name,
         'Anonymous'
     ) AS host_name,
--- models/dim/dim_listings_cleansed.sql
+++ models/dim/dim_listings_cleansed.sql
@@ -6,7 +6,7 @@
 WITH src_listings AS (
     SELECT * FROM {{ ref('src_listings') }}
 )
-SELECT 
+SELECT
   listing_id,
   listing_name,
   room_type,
@@ -15,14 +15,8 @@ SELECT
     ELSE minimum_nights
   END AS minimum_nights,
   host_id,
-  REPLACE(
-    price_str,
-    '$'
-  ) :: NUMBER(
-    10,
-    2
-  ) AS price,
+  REPLACE(price_str, '$', '')::NUMERIC(10, 2) AS price,
   created_at,
   updated_at
-FROM
-  src_listings
+FROM src_listings
+
